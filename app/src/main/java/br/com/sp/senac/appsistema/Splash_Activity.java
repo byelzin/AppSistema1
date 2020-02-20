be620271a1eb6ec44abeebab196ed307a7c70b7d
GABRIEL DOS SANTOS LEITE <gabriel.sleite2@senacsp.edu.br>
2020-02-20 11:39:07 -0300

    Initial commit

---
+++ app/src/main/java/br/com/sp/senac/appsistema/Splash_Activity.java
@@ -0,0 +1,32 @@
+package br.com.sp.senac.appsistema;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Toast;
+
+public class Splash_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_layout);
+//metodo para rodar processo em tempo determindo
+        Handler handler = new Handler();
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+              abriJanela();
+            }
+        },5000);
+    }
+
+    public void abriJanela() {
+       // Toast.makeText(getApplicationContext(),"Clique na imagem!!!",Toast.LENGTH_SHORT).show();
+        Intent abrir = new Intent(Splash_Activity.this,Login_Activity.class);startActivity(abrir);
+    }
+}
